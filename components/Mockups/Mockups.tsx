7a430217bb965648353de45d53bd192b69cc10f6
Mhttt <74661052+Mhttt@users.noreply.github.com>
2023-08-21 19:16:36 +0200

    added right side

---
+++ components/Mockups/Mockups.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import styles from './Mockups.module.css';
+
+export default function Mockups() {
+  return (
+    <div className={styles.container}>
+      <Image className={styles.img1} src="/mockup.png" alt="test" width={500} height={500}></Image>
+      <Image className={styles.img2} src="/mockup.png" alt="test" width={500} height={500}></Image>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import InfoSection from '../components/InfoSection/InfoSection';
 import Menu from '../components/Menu/Menu';
 import ProgressBar from '../components/ProgressBar/ProgressBar';
@@ -5,12 +6,10 @@ import ScrollTip from '../components/ScrollTip/ScrollTip';
 import Step from '../components/Step/Step';
 import StepSection from '../components/StepSection/StepSection';
 import styles from './index.module.css';
+import Mockups from '../components/Mockups/Mockups';
 
 export default function Page() {
-
-  const testData = [
-    { bgcolor: "#ef6c00", completed: 70 },
-  ];
+  const testData = [{ bgcolor: '#ef6c00', completed: 70 }];
 
   return (
     <div className={styles.outerContainer}>
@@ -18,11 +17,38 @@ export default function Page() {
         <Menu></Menu>
         <InfoSection></InfoSection>
         <StepSection></StepSection>
-          {testData.map((item, idx) => (
-        <ProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} />
-      ))}
+        {testData.map((item, idx) => (
+          <ProgressBar
+            key={idx}
+            bgcolor={item.bgcolor}
+            completed={item.completed}
+          />
+        ))}
       </div>
-      <div className={styles.rightContainer}></div>
+      <div className={styles.rightContainer}>
+        <Image
+          className={styles.fadeIn}
+          src="/circle1.svg"
+          alt="test"
+          width={200}
+          height={500}
+        ></Image>
+        <Image
+          src="/circle2.svg"
+          alt="test"
+          width={200}
+          height={200}
+          className={`${styles.circle2} ${styles.fadeIn}`}
+        ></Image>
+        <Image
+          src="/circle3.svg"
+          alt="test"
+          width={700}
+          height={150}
+          className={`${styles.circle3} ${styles.fadeIn}`}
+        ></Image>
+        <Mockups></Mockups>
+      </div>
       <ScrollTip></ScrollTip>
     </div>
   );
